025c05a387ff5661c4e4185c43770a8edb1731d8
RookieJoel <jojackchanah@gmail.com>
2025-08-29 10:55:51 +0700

    feat: added config and modified .env.example

---
+++ apps/config/config.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+interface EnvConfig {
+  PORT: number; 
+  DB_URL: string;
+  ACCESSTOKEN_SECRET: string;
+  REFRESHTOKEN_SECRET: string;
+  PASSWORD_SALT_ROUNDS: number;
+  NODE_ENV: string;
+
+}
+
+export const config:EnvConfig = {
+    PORT: Number(process.env.PORT),
+    DB_URL: process.env.DATABASE_URL as string,
+    ACCESSTOKEN_SECRET: process.env.ACCESSTOKEN_SECRET as string,
+    REFRESHTOKEN_SECRET: process.env.REFRESHTOKEN_SECRET as string,
+    PASSWORD_SALT_ROUNDS: Number(process.env.PASSWORD_SALT_ROUNDS),
+    NODE_ENV: process.env.NODE_ENV || 'development',
+};
