1391622fb02e34ffd06e333a3770d042171eae75
6630360621 <6630360621@student.chula.ac.th>
2025-09-03 20:30:55 +0700

    finish createNewGroup in groupService and finish createNewGroup in groupRepository

--- apps/services/group/groupService.ts
+++ apps/services/group/groupService.ts
@@ -1,6 +1,6 @@
-import { Interest } from "@/prisma/index";
 import { GroupRepository } from "@/repository/Group/groupRepository";
 import { AppError } from "@/types/error/AppError";
+import { groupCreateReq } from "@/types/group/groupRequest";
 
 export class GroupService {
     private grouprepository: GroupRepository;
@@ -9,7 +9,7 @@ export class GroupService {
         this.grouprepository = new GroupRepository();
     }
 
-    async createNewGroup(group_data: any) {
+    async createNewGroup(group_data: groupCreateReq) {
         try {
             const newGroup = await this.grouprepository.createNewGroup(group_data);
             return newGroup;
