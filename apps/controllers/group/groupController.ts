2bfc14f921382fd7aa4a1df4913aa596ba4c743a
6630360621 <6630360621@student.chula.ac.th>
2025-09-03 20:34:53 +0700

    finish createGroup in groupController

--- apps/controllers/group/groupController.ts
+++ apps/controllers/group/groupController.ts
@@ -1,44 +1,19 @@
 import { Request, Response } from "express";
 import { AppError } from "@/types/error/AppError";
-#import { GroupService } from "@/services/group/groupService";
-import { Interest } from "@/prisma/index";
+import { GroupService } from "@/services/group/groupService";
+
 export class GroupController {
-    private service: UserService;
+    private groupService: GroupService;
 
     constructor() {
-        this.service = new UserService();
+        this.groupService = new GroupService();
     }
 
-    async getInterests(req: Request, res: Response): Promise<void> {
+    async createGroup(req: Request, res: Response): Promise<void> {
         try {
-            const _email = req.body._email;
-            const interests = await this.service.getUserInterests(_email);
-            res.status(200).json({ _email: _email, interests: interests });
-        } catch (error: unknown) {
-            if (error instanceof AppError) {
-                res.status(error.statusCode).json({ message: error.message });
-                return;
-            }
-
-            console.error("Unexpected error:", error);
-            res.status(500).json({ message: "Internal server error" });
-        }
-    }
-
-    async patchInterests(req: Request, res: Response): Promise<void> {
-        try {
-            const _email = req.body._email;
-            const { interests } = req.body;
-
-            const updated = await this.service.updateUserInterests(
-                _email,
-                interests
-            );
-            res.status(200).json({
-                _email: _email,
-                message: "Interests updated",
-                updated,
-            });
+            const groupData = req.body;
+            const newGroup = await this.groupService.createNewGroup(groupData);
+            res.status(201).json(newGroup);
         } catch (error: unknown) {
             if (error instanceof AppError) {
                 res.status(error.statusCode).json({ message: error.message });
