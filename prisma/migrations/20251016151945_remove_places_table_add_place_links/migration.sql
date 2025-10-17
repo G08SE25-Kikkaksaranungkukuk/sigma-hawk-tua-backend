83dec301f72d9ba76309ed4a66c701e552992cb5
Thanakrit_B <thanakirt47@gmail.com>
2025-10-17 22:47:54 +0700

    Fix Migrate

--- prisma/migrations/20251016151945_remove_places_table_add_place_links/migration.sql
+++ prisma/migrations/20251016151945_remove_places_table_add_place_links/migration.sql
@@ -1,13 +1,16 @@
 /*
   Warnings:
 
-  - You are about to drop the `itinerary_places` table. If the table is not empty, all the data it contains will be lost.
   - You are about to drop the `place_interests` table. If the table is not empty, all the data it contains will be lost.
   - You are about to drop the `places` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_CreateEditItinerary` table. If the table is not empty, all the data it contains will be lost.
 
 */
 -- DropForeignKey
-ALTER TABLE "public"."itinerary_places" DROP CONSTRAINT "itinerary_places_itinerary_id_fkey";
+ALTER TABLE "public"."_CreateEditItinerary" DROP CONSTRAINT IF EXISTS "_CreateEditItinerary_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."_CreateEditItinerary" DROP CONSTRAINT IF EXISTS "_CreateEditItinerary_B_fkey";
 
 -- DropForeignKey
 ALTER TABLE "public"."place_interests" DROP CONSTRAINT "place_interests_interest_id_fkey";
@@ -19,7 +22,7 @@ ALTER TABLE "public"."place_interests" DROP CONSTRAINT "place_interests_place_id
 ALTER TABLE "public"."itineraries" ADD COLUMN     "place_links" TEXT[] DEFAULT ARRAY[]::TEXT[];
 
 -- DropTable
-DROP TABLE "public"."itinerary_places";
+DROP TABLE IF EXISTS "public"."_CreateEditItinerary";
 
 -- DropTable
 DROP TABLE "public"."place_interests";
