b0999d1f121d1bdf7cdfeaa4f1012cb6a79dcff1
6630360621 <6630360621@student.chula.ac.th>
2025-10-18 20:11:38 +0700

    Revert "add title and desc into return data in getBlogContent"
    This reverts commit e7b291c590fc0ee56073bb445fd3cb1c69bdc216.

--- apps/repository/Blog/blogRepository.ts
+++ apps/repository/Blog/blogRepository.ts
@@ -217,18 +217,13 @@ export class BlogRepository {
         })
     }
 
-    async getBlogContent(blog_id: string): Promise<{ title: string; description: string | null; html_output: string }> {
-        const blogContent = await prisma.blog.findFirstOrThrow({
-            where: {
-                "blog_id": Number(blog_id)
-            },
-            select: {
-                title: true,
-                description: true,
-                html_output: true
+    async getBlogContent(blog_id : string) : Promise<string> {
+        const ret = await prisma.blog.findFirstOrThrow({
+            where : {
+                "blog_id" : Number(blog_id)
             }
-        });
-        return blogContent;
+        })
+        return ret.html_output
     }
 
     /**
