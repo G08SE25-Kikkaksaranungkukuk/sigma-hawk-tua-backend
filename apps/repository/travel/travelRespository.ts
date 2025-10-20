a1a9ede9a6cae62798032a3e8c915805e549dbc0
Amorn Phanturat 6632250621 <6632250621@student.chula.ac.th>
2025-10-20 21:31:19 +0700

    added minor fix to the travelRepository

--- apps/repository/travel/travelRespository.ts
+++ apps/repository/travel/travelRespository.ts
@@ -3,6 +3,15 @@ import { prisma } from "@/config/prismaClient";
 export class TravelRepository {
     async getTravelHistory(user_id : number) {
         const user_groups_itineraries = await prisma.group.findMany({
+            where : {
+                'Belongs' : {
+                    'some' : {
+                        User : {
+                            user_id : user_id
+                        }
+                    }
+                }
+            },
             include : {
                 itineraries : true
             }
