7246cba3b1dc3e6ec42b431df0433ae8d8e52de1
Tonnamgg <jirapat.dkk@gmail.com>
2025-11-09 10:53:08 +0700

    remove someapi

--- apps/routes/v1/RatingRouter.ts
+++ apps/routes/v1/RatingRouter.ts
@@ -32,28 +32,7 @@ export class RatingRouterV1 extends BaseRouter {
       this.ratingController.getUserRating.bind(this.ratingController)
     );
 
-    // GET /user/:userId/rating/stats - Get rating statistics for target user (public)
-    this.router.get('/user/:userId/rating/stats',
-      validateUserId,
-      this.ratingController.getUserRatingStats.bind(this.ratingController)
-    );
 
-    // GET /user/:userId/rating/all - Get all ratings received by target user (public)
-    this.router.get('/user/:userId/rating/all',
-      validateUserId,
-      this.ratingController.getAllUserRatings.bind(this.ratingController)
-    );
-
-    // GET /user/:userId/rating/detailed-stats - Get detailed rating statistics for target user (public)
-    this.router.get('/user/:userId/rating/detailed-stats',
-      validateUserId,
-      this.ratingController.getDetailedUserRatingStats.bind(this.ratingController)
-    );
-
-    // GET /leaderboard - Get top rated users leaderboard (public)
-    this.router.get('/leaderboard',
-      this.ratingController.getTopRatedUsers.bind(this.ratingController)
-    );
 
     // Authenticated routes (require authentication)
     this.router.use(authMiddleware);
@@ -82,11 +61,7 @@ export class RatingRouterV1 extends BaseRouter {
       this.ratingController.deleteRating.bind(this.ratingController)
     );
 
-    // GET /user/:userId/rating/check - Check if authenticated user has rated target user
-    this.router.get('/user/:userId/rating/check',
-      validateUserId,
-      this.ratingController.checkIfRated.bind(this.ratingController)
-    );
+
   }
 
   public getRouter(): Router {
